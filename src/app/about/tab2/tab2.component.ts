1adb2214bdfe7502c67d1dcd55c49c9edc7d3b66
Emir Ale <emirderespaldo@gmail.com>
2022-01-06 21:53:46 -0300

    General Styles

--- src/app/about/tab2/tab2.component.ts
+++ src/app/about/tab2/tab2.component.ts
@@ -7,13 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab2Component implements OnInit {
   technologies = [
-    'Angular 9',
-    'Javascript',
-    'Bootstrap 4',
-    'Github',
-    'Typescript',
-    'CSS',
+    {
+      name: 'Angular 13',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png'
+    },
+    {
+      name: 'Javascrip',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Javascript_badge.svg/946px-Javascript_badge.svg.png'
+    },
+    {
+      name: 'Bootstrap 4',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1200px-Bootstrap_logo.svg.png'
+    },
+    {
+      name: 'Github',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png'
+    },
+    {
+      name: 'Typescript',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/1200px-CSS.3.svg.png'
+    },
+    {
+      name: 'Scss',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/2560px-Sass_Logo_Color.svg.png'
+    }
   ];
+
 
   constructor() { }
 
